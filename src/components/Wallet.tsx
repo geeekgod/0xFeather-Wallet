212fe812af6074270d1dc61c511d70e0ea65d86d
Rishabh Singh <ris8645208@gmail.com>
2023-10-28 22:55:32 +0530

    feat: add load balance

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,31 +1,57 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createWallet } from '../lib/createWallet';
-import { ethers } from 'ethers';
+import { ethers, JsonRpcProvider } from 'ethers';
 import { User } from '@prisma/client';
 
 type UserType = Omit<User, 'password'>
 
 const Wallet = (user: UserType) => {
+  const provider = new JsonRpcProvider(
+    process.env.NEXT_PUBLIC_INFURA_URL,
+  );
   const [wallet, setWallet] = useState<ethers.Wallet | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [balance, setBalance] = useState<string | null>(null);
 
-  const handleCreateWallet = async () => {
+  useEffect(() => {
 
-    // set wallet from user's private key and address
-    if (user.ethereumPrivKey && user.ethereumAddress) {
-      const newWallet = new ethers.Wallet(user.ethereumPrivKey);
-      setWallet(newWallet);
-      return;
+    const saveWallet = async (wallet: ethers.Wallet) => {
+      const res = await fetch('/api/wallet', {
+        method: 'POST',
+        body: JSON.stringify({
+          ethereumAddress: wallet.address,
+          ethereumPrivKey: wallet.privateKey,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': user.id.toString(),
+        },
+      });
+
+      if (!res.ok) {
+        setError((await res.json()).message);
+        return;
+      }
     }
 
-    const walletData = await createWallet();
-    const newWallet = new ethers.Wallet(walletData.privateKey);
-    setWallet(newWallet);
-    saveWallet(newWallet);
-  };
+    const handleCreateWallet = async () => {
+
+      // set wallet from user's private key and address
+      if (user.ethereumPrivKey && user.ethereumAddress) {
+        const newWallet = new ethers.Wallet(user.ethereumPrivKey);
+        setWallet(newWallet);
+        return;
+      }
+
+      const walletData = await createWallet();
+      const newWallet = new ethers.Wallet(walletData.privateKey);
+      setWallet(newWallet);
+      saveWallet(newWallet);
+    };
+    handleCreateWallet();
+  }, []);
 
   // send address & private key to backend at /api/wallet/new using fetch
   const saveWallet = async (wallet: ethers.Wallet) => {
@@ -47,16 +73,37 @@ const Wallet = (user: UserType) => {
     }
   }
 
+  const fetchBalance = async () => {
+    if (!wallet) return;
+    if (!wallet.provider) return;
+    if (!wallet.address) return;
+
+    const balance = await wallet.provider.getBalance(wallet.address);
+
+    const etherString = ethers.formatEther(balance);
+    setBalance(etherString);
+  }
+
+  useEffect(() => {
+    if (!wallet) return;
+
+    if (!wallet.provider) {
+      const newWallet = wallet.connect(provider);
+      setWallet(newWallet);
+    };
+    fetchBalance();
+  }, [wallet]);
 
   return (
     <div>
-      <button onClick={handleCreateWallet}>Create Ethereum Wallet</button>
       {error && <p>{error}</p>}
       {wallet && (
         <div>
           <h3>Wallet Details:</h3>
           <p>Address: {wallet.address}</p>
           <p>Private Key: {wallet.privateKey}</p>
+          <p>Balance: {balance}</p>
+          <button onClick={fetchBalance}>Refresh Balance</button>
         </div>
       )}
     </div>
