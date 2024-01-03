83e07a278f97e392d4e0384c5eaa64407fc9aaba
Rishabh Singh <ris8645208@gmail.com>
2024-01-04 01:02:55 +0530

    fix: layout issue

--- src/app/auth/sign_in/page.tsx
+++ src/app/auth/sign_in/page.tsx
@@ -2,7 +2,7 @@
 import { signIn } from "next-auth/react";
 import { useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export default function SignIn() {
 
@@ -48,12 +48,18 @@ export default function SignIn() {
     setFormValues({ ...formValues, [name]: value });
   };
 
+  useEffect(() => {
+    if (error) {
+      setError("");
+    }
+  }, [formValues]);
+
 
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-between">
       <div className="bg-slate-900 w-full h-full">
         <div className="bg-gradient-to-b from-violet-600/[.15] via-transparent flex flex-col h-screen justify-center items-center">
-          <div className="pt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700 w-[50%]">
+          <div className="pt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700 w-[90%] md:w-[70%] lg:w-[50%]">
             <div className="p-4 sm:p-7">
               <div className="text-center">
                 <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
@@ -82,11 +88,14 @@ export default function SignIn() {
                           onChange={handleChange}
                           placeholder="Email address"
                           className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="email-error" />
-                        <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                          <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                          </svg>
-                        </div>
+                        {
+                          error && error === "Invalid email or password" &&
+                          <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                            <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                            </svg>
+                          </div>
+                        }
                       </div>
                       <p className="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
                     </div>
@@ -104,11 +113,14 @@ export default function SignIn() {
                           onChange={handleChange}
                           placeholder="Password"
                           className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" required aria-describedby="password-error" />
-                        <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
-                          <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                          </svg>
-                        </div>
+                        {
+                          error && error === "Invalid email or password" &&
+                          <div className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                            <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                            </svg>
+                          </div>
+                        }
                       </div>
                       <p className="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
                     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-between">
       <div className="bg-slate-900 w-full h-full">
-        <div className="bg-gradient-to-b from-violet-600/[.15] via-transparent">
-          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+        <div className="bg-gradient-to-b from-violet-600/[.15] via-transparent h-full">
+          <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8 h-full flex flex-col items-center justify-center gap-4">
             <div className="flex justify-center">
               <Link className="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 pl-4 rounded-full shadow-md" href="/">
                 <p className="mr-2 inline-block text-white text-sm">
@@ -32,7 +32,7 @@ export default function Home() {
             <div className="text-center">
               <Link href='/app' className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800">
                 Get started
-                <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <svg className="w-2.5 h-2.5" width="26" height="26" viewBox="0 0 16 16" fill="none">
                   <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                 </svg>
               </Link>
